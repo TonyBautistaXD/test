8a859c880612425a98c8a54b55d9f8543ad98d7f
TonyBautistaXD <113541314+TonyBautistaXD@users.noreply.github.com>
2024-04-08 07:17:33 -0600

    Update variables.tf

--- utils/variables.tf
+++ utils/variables.tf
@@ -34,26 +34,25 @@ variable "ami" {
 variable "bucket_name" {
  type        = string
  description = "Bucket name for project data (model files, csv files, etc.)"
- default     = "predictiaxaldigital"
+ default     = "predictiadata"
 }
 
 variable "accessKeyId" {
  type        = string
  description = "accessKeyId AWS"
- default     = "ASIAW3MEEUHHGMDUL56Z"
+ default     = "xxxx"
 }
 
 variable "secretAccessKey" {
  type        = string
  description = "secretAccessKey AWS"
- default     = "uJAtiSxusCn9wd7l+gMlZ6ZdummbN08kr5+dJcVI"
+ default     = "xxxx"
 }
 
 variable "sessionToken" {
  type        = string
  description = "sessionToken AWS"
- default     = "IQoJb3JpZ2luX2VjEJz//////////wEaCXVzLWVhc3QtMSJHMEUCIADrx+C2IWnY4qtsseacUDPl9tFbd5xoA145EGLK+ho0AiEAjEagGdC1mfzTuooRTTC3HgZ6QgpuDUUXekcRXHX2OcIqsQMIxf//////////ARAAGgw0NzExMTI5MTc0NTQiDCHAsMyniQysqEUweiqFA/mfUDQVQIC4bFeA4bzJhoI6iwlKQbmnl+8pTDJ+kHlpk28WzjqwVzIYRGnKRuHilEeosQqJgp2AnZhUdugF1fCpKMlaHLorHQhqil8EdjP8+0fCbmZXqm89BdYLwJTcCSI9eHOFMnYrbnGoDoujiaplPb+fCC+6jyEUvCAVgDIivA13j2HpRbxRXElzeoB0h/va+u/kIPUiPNDw2lWGhJz1KiFc83F+XJcVNIrQajoDqP6ZzPKB16+d1qLMHNiIrW2EyXB74YCINlWQ8QT9/l+J/jRI4KaqD6T1CnyxhCKzb5xu1lkJDsxub9lLOJdzq1modeFLXMy50bReqGMS+2EyUGvEMYF7CmObFheprX89Oy+X6NYj5yIHL+rYe6NeToaPFhaLiMI/yHxNVKSXbnv5GlfRXyNOyfmwBn+D0ad+xHkM88/4tGUL/bGFuErWrPR5Df43HJKyZOfCqJkPp+nFtopYj0j7OCll+v/1YVLdLbRO0R/TfuhfMZNb8MAfILrfTrvuMKrPxrAGOqYBlJB0T7BKM1u+f9Q5B3YPH0csaludqfuQH5RKvYv24jH27Pn2xwxKdvLqf0DDMnSvTrov6I4YsskjMKC4q7dZpyn7/IFsaX6qjPFR+jC2r9Ym9MSaIBq4+4kgwtYmUXFUbQm8jzHkLi7XF6Zhw9epiOBg9oodZrXURt8MLusKPd3lRdUPrR6DncX3rIABQoCWN8vUnXRAV4jcanFYmXi7dbxUrXCvdA=="
-}
+ default     = "xxxx"}
 
 variable "ssh_key_pair_name" {
  type        = string
@@ -64,7 +63,7 @@ variable "ssh_key_pair_name" {
 variable "github_repo" {
  type        = string
  description = "Repo with the Flask code. Change remote exec to use private repo"
- default     = "https://github.com/kandreyrosales/predictia.git"
+ default     = "https://github.com/TonyBautistaXD/test.git"
 }
 
 variable "private_key_ec2_path" {
